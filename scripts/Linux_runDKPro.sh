a3659120fd74bcc5490352e00cbda53b97c0aa89
galanisd <galanisd@gmail.com>
2017-05-29 13:53:06 +0300

    Corrected scripts.

--- scripts/Linux_runDKPro.sh
+++ scripts/Linux_runDKPro.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx4096m -jar -Dfile.encoding=UTF-8  $(pwd)"/../omtd-simple-wokflows-dkpro/target/omtd-simple-workflows-dkpro-0.0.1-SNAPSHOT-exec.jar" $@
+java -Xmx4096m -jar -Dfile.encoding=UTF-8  $(pwd)"/../omtd-simple-workflows-dkpro/target/omtd-simple-workflows-dkpro-0.0.1-SNAPSHOT-exec.jar" $@
--- scripts/Winds_runDKPro.bat
+++ scripts/Winds_runDKPro.bat
@@ -1,2 +1,2 @@
-java -Xmx4096m -jar -Dfile.encoding=UTF-8 %CD%/../omtd-simple-wokflows-dkpro/target/omtd-simple-workflows-dkpro-0.0.1-SNAPSHOT-exec.jar  %*
+java -Xmx4096m -jar -Dfile.encoding=UTF-8 %CD%/../omtd-simple-workflows-dkpro/target/omtd-simple-workflows-dkpro-0.0.1-SNAPSHOT-exec.jar  %*
 pause;
