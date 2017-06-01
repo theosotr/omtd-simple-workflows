31f83c385ed9b0b0793d38e21e3237a8de4d3313
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2017-06-01 15:13:06 +0300

    Change the test class for NER in SSH to work on OMTD demo data

--- omtd-simple-wokflows-dkpro/src/main/java/eu/openminted/simplewokflows/dkpro/PipelineNERInferenceForPDFs.java
+++ omtd-simple-wokflows-dkpro/src/main/java/eu/openminted/simplewokflows/dkpro/PipelineNERInferenceForPDFs.java
@@ -27,33 +27,25 @@ public class PipelineNERInferenceForPDFs {
 	     if(!outDirF.exists()){
 	    	 outDirF.mkdirs();
 	     }
-	     File outDirF_xmi = new File(outDir+"/xmi");
-	     if(!outDirF_xmi.exists()){
-	    	 outDirF_xmi.mkdirs();
-	     }
-	     File outDirF_xmi_ann = new File(outDir+"/xmi_annotated");
-	     if(!outDirF_xmi_ann.exists()){
-	    	 outDirF_xmi_ann.mkdirs();
-	     }
-	     
+	        
 		 
 	     // PDF to XMI
 		 logger.info("Setting parameters for PDF to XMI transformation");
 	     eu.openminted.uc.socialsciences.io.pdfx.Pipeline pipelinePdf2Xmi = new eu.openminted.uc.socialsciences.io.pdfx.Pipeline();
 	     pipelinePdf2Xmi.setInput(inputDir);
-	     pipelinePdf2Xmi.setOutput(outDir+"/xmi");
+	     pipelinePdf2Xmi.setOutput("/tmp/");	   
 	     pipelinePdf2Xmi.setLanguage(eu.openminted.uc.socialsciences.io.pdfx.PdfxXmlToXmiConverter.LANGUAGE_CODE_EN);
 	     pipelinePdf2Xmi.setHomePath(dictionaries.toString());
 	     pipelinePdf2Xmi.setOverwriteOutput(true);
 	     logger.info("Running PDF to XMI transformation");
 	     pipelinePdf2Xmi.run();
 	     logger.info("PDF to XMI transformation finished");
-	        
+	       
 	     //  NER
 	     logger.info("Setting parameters for NER");
 	     Pipeline pipelineNER = new Pipeline();
-	     pipelineNER.setInput(outDir+"/xmi" + "/*.xmi");
-	     pipelineNER.setOutput(outDir+"/xmi_annotated");
+	     pipelineNER.setInput("/tmp/*.xmi");
+	     pipelineNER.setOutput(outDir);
 	     pipelineNER.setUseStanfordModels(true);
 	     logger.info("Running NER");
 	     pipelineNER.run();
--- omtd-simple-wokflows-dkpro/src/main/java/eu/openminted/simplewokflows/dkpro/test/PipelineNERInferenceForPDFsRunnerTest.java
+++ omtd-simple-wokflows-dkpro/src/main/java/eu/openminted/simplewokflows/dkpro/test/PipelineNERInferenceForPDFsRunnerTest.java
@@ -1,6 +1,8 @@
 package eu.openminted.simplewokflows.dkpro.test;
 
 
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.LoggerFactory;
 import ch.qos.logback.classic.Level;
 import eu.openminted.simplewokflows.dkpro.PipelineNERInferenceForPDFs;
@@ -17,12 +19,25 @@ public class PipelineNERInferenceForPDFsRunnerTest {
 	 	ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger)LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
 	 	rootLogger.setLevel(Level.toLevel("info"));
 		
+	 	long startTime = System.currentTimeMillis();
+	 	
 	 	// Parameters
-	 	String input = "/home/gkirtzou/Desktop/tmp/pdf";
-	 	String output = "/home/gkirtzou/Desktop/tmp/output";
-	 			
+	 	String input = "/home/gkirtzou/Dropbox/Work/Projects/OpenMinded/Data/2017.06_OMTD_Demo/Dataset2/fulltext/";
+	 	String output = "/home/gkirtzou/Dropbox/Work/Projects/OpenMinded/Data/2017.06_OMTD_Demo/WF3_Results/";
+	 	
+	 	// Running NER inference
 	 	String [] myargs = {input, output};
     	PipelineNERInferenceForPDFs.NERInferenceForPDFs(myargs);
+    	
+    	long endTime   = System.currentTimeMillis();
+	 	long totalTime = endTime - startTime;
+	    long hours = TimeUnit.MILLISECONDS.toHours(totalTime);
+	    totalTime -= TimeUnit.HOURS.toMillis(hours);
+	    long minutes = TimeUnit.MILLISECONDS.toMinutes(totalTime);
+	    totalTime -= TimeUnit.MINUTES.toMillis(minutes);
+	    long seconds = TimeUnit.MILLISECONDS.toSeconds(totalTime);
+
+	    rootLogger.info("Total running time " + hours + "H:" + minutes + "M:" + seconds + " S");
 
 	}
 
