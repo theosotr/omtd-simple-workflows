254590c70ff110115c5f4a564d99824540c71e48
Katerina Gkirtzou <katerina.gkirtzou@gmail.com>
2017-06-09 12:38:07 +0300

    Fixing details in project depedencies for NER in SSH

--- installProjectsDepedencies.sh
+++ installProjectsDepedencies.sh
@@ -13,6 +13,7 @@ mvn clean install
 cd ..
 git clone https://github.com/gkirtzou/uc-tdm-socialsciences.git
 cd uc-tdm-socialsciences
+git checkout omtd-demo
 mvn clean install -Dmaven.test.skip=true
 
 cd ..
