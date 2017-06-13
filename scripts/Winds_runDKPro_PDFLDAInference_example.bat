e8f36329af6b80879159743c22b1587a74915745
galanisd <galanisd@gmail.com>
2017-06-13 18:37:21 +0300

    Corrected output folder.

--- scripts/Winds_runDKPro_PDFLDAInference_example.bat
+++ scripts/Winds_runDKPro_PDFLDAInference_example.bat
@@ -1,1 +1,1 @@
-Winds_runDKPro.bat eu.openminted.simplewokflows.dkpro.PipelineLDAInferenceForPDFs ../testInput/ C:/Users/galanisd/Desktop/Data/CORE/_COREMalletScripts/20160408_115129model.txt ../testOutput/
+Winds_runDKPro.bat eu.openminted.simplewokflows.dkpro.PipelineLDAInferenceForPDFs ../testInput/ C:/Users/galanisd/Desktop/Data/CORE/_COREMalletScripts/20160408_115129model.txt ../testOutput3/
